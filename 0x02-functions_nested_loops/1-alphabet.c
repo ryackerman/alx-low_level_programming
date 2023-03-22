bb52c77a63a78412074b724075583e63c4b9ae99
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-03-21 17:59:55 -0700

    small change

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,6 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
+
 void print_alphabet(void)
 {
 	char c;
@@ -13,5 +14,5 @@ void print_alphabet(void)
 	{
 		_putchar(c);
 	}
-	_putchar(c);
+	_putchar('\n');
 }
