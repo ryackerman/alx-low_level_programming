7271e206a48e665743bc28cc5c96f4eb44891bb6
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-03-23 19:40:47 -0700

    small changes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -19,11 +20,11 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (i % 3 == 0)
+		else if (i % 3 == 0 && !(i % 5 == 0))
 		{
 			printf("Fizz ");
 		}
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && !(i % 3 == 0))
 		{
 			printf("Buzz ");
 		}
