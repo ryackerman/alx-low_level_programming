f939722f137b850f89eb21520ae3f0269025ca34
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-03-23 20:09:52 -0700

    extra coding

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,7 +8,7 @@
 
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if (size <= 0)
 	{
@@ -16,9 +16,13 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (i = 1; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 1; j <= i; j++)
+			for (j = 0; j < size - i - 1; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k <= i; k++)
 			{
 				_putchar('#');
 			}
