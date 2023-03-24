7e3f9ee088d31a0c2877a29e5bd8a58076a0f77d
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-03-23 19:44:29 -0700

    small changes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 99; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
@@ -29,7 +29,7 @@ int main(void)
 			printf("Buzz ");
 		}
 		else
-			printf("%d", i);
+			printf("%d ", i);
 	}
 	printf("\n");
 
