d9b71a8fa000953e89be19573cbc8962ec46db9a
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-03-23 20:03:19 -0700

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ * Return: Always 0 (Success)
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
