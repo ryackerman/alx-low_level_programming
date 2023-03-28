0d3ab34bb0ed5ab4788a737be4e92bd7ac6a9f88
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-03-27 17:34:00 -0700

    small changes

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,14 @@ void print_rev(char *s)
 {
 	int i, len = 0;
 
-	while (*str != '\0')
+	while (s[len] != '\0')
 	{
 		len++;
-		str++;
 	}
 
 	for (i = len - 1; i >= 0; i--)
 	{
-		_putchar(*(str + i));
+		_putchar(*(s + i));
 	}
 	_putchar('\n');
 }
