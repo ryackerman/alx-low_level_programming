594b8a8d6a37e9da6c7803a13db3bfcbdd8b03da
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-04-11 16:22:56 -0700

    fixing malloc errors

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,18 +11,24 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **grid, i, j;
+	int **grid;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 
-	grid = (int **) malloc(height * sizeof(int *));
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+	{
+		free(grid);
+		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *) malloc(width * sizeof(int));
+		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
