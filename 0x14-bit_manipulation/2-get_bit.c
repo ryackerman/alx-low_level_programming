8a12ca614552fd5275d253262e7f9177a1df121f
Khalid <t.khalidtaleb@gmail.com>
2023-05-04 14:40:24 -0700

    fixing a requirement

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,5 +13,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	return (n >> index) & 1;
+	if ((n & (1 << index)) == 0)
+		return (0);
+	return (1);
 }
